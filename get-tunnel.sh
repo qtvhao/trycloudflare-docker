01af35d69b1e5de118fbb7fdb45ce3398c4c947a
Your Name <you@example.com>
2023-10-21 10:21:11 +0000

    add

--- get-tunnel.sh
+++ get-tunnel.sh
@@ -9,7 +9,7 @@ if [ -z "$NETWORK" ]; then
     NETWORK="tryc"
 fi
 export NETWORK
-export CONTAINER_NAME="$project_name-$NETWORK-"$(echo "$project_name-$NETWORK-$ADDRESS" | base64 | tr -d '=' | tr -d '\n' | tr -d '\r' | cut -c1-5)
+export CONTAINER_NAME="$project_name-$NETWORK-"$(echo "$project_name-$NETWORK-$ADDRESS" | md5sum | tr -d '=' | tr -d '\n' | tr -d '\r' | cut -c1-5)
 docker network inspect $NETWORK >/dev/null 2>&1 || docker network create $NETWORK >/dev/null 2>&1
 sh get-exist-tunnel.sh "$docker_compose_file" "$project_name-$NETWORK" > /dev/null || sh start-tunnel.sh "$docker_compose_file" "$project_name-$NETWORK" > /dev/null
 sh get-exist-tunnel.sh "$docker_compose_file" "$project_name-$NETWORK"
