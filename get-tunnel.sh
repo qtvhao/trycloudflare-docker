166bd662a9930ad3003d8bd129de73f03df230c2
Your Name <you@example.com>
2023-10-21 05:17:22 +0000

    add

--- get-tunnel.sh
+++ get-tunnel.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 project_name="$1"
 docker_compose_file=$2
+NETWORK="$3"
 if [ -z "$docker_compose_file" ]; then
     docker_compose_file="docker-compose.yml"
 fi
-sh get-exist-tunnel.sh "$docker_compose_file" "$project_name" > /dev/null || sh start-tunnel.sh "$docker_compose_file" "$project_name" > /dev/null
-sh get-exist-tunnel.sh "$docker_compose_file" "$project_name"
+if [ -z "$NETWORK" ]; then
+    NETWORK="trycloudflare-docker"
+fi
+export NETWORK
+docker network inspect $NETWORK >/dev/null 2>&1 || docker network create $NETWORK >/dev/null 2>&1
+sh get-exist-tunnel.sh "$docker_compose_file" "$project_name-$NETWORK" > /dev/null || sh start-tunnel.sh "$docker_compose_file" "$project_name-$NETWORK" > /dev/null
+sh get-exist-tunnel.sh "$docker_compose_file" "$project_name-$NETWORK"
